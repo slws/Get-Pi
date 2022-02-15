b5f417d115176222f945407b7f2bcf11e7215a86
Simon <slws@users.noreply.github.com>
2022-02-15 10:39:58 +0000

    Function Efficiency
    Small efficiency improvements, commented out unnecessary output, moved stamps to directly before/after while loop. Further efficiency gains will likely require selection of a different algorithm, renamed some variables for clarity.

--- Get-Pi.ps1
+++ Get-Pi.ps1
@@ -15,30 +15,30 @@ Param (
     } Else {
         Write-Output "Number of decimal places (DP) required. e.g.: Get-Pi 6"
     }
-    $1stStamp = Get-Date
     $piCharList = New-Object System.Collections.Generic.List[System.Object]
     $piChar = 0
     $divisor = 3
-    $qtrPi = 1
+    $qtrPiINT = 1
     $piCount = 1
-    [String]$pi = 0
+    [String]$piString = 0
+    $1stStamp = Get-Date
     While($piCharList.count -lt $($dpReq+2)){
-        $last1 = $pi
+        $lastPiString = $piString
         If(($piCount % 2) -eq 1){
-            $qtrPi = $qtrPi - 1/$divisor
+            $qtrPiINT = $qtrPiINT - (1/$divisor)
         } Else {
-            $qtrPi = $qtrPi + 1/$divisor
+            $qtrPiINT = $qtrPiINT + (1/$divisor)
         }
-        $pi = $($qtrPi * 4).ToString()
-        #Write-Output "$pi"
-        If($pi.ToCharArray()[$piChar] -eq $last1.ToCharArray()[$piChar]){
-            $piCharList.add($pi.ToCharArray()[$piChar])
+        $piString = $($qtrPiINT * 4).ToString()
+        #Write-Output "$piString"
+        If($piString.ToCharArray()[$piChar] -eq $lastPiString.ToCharArray()[$piChar]){
+            $piCharList.add($piString.ToCharArray()[$piChar])
             If($piCharList.count -gt 2){
                 $dp = $piChar - 1
-                If($dp -lt $dpReq){
-                    Write-Output "Found $dp DP: $piCharList"
-                } Else {
-                    Write-Output "`r`nPi to $dp DP is: `r`n$piCharList"
+                If($dp -eq $dpReq){
+                    Write-Output "`r`nPi to $dp DP is: `r`n$piCharList`r`n"
+                #} Else {
+                #    Write-Output "Found $dp DP: $piCharList"
                 }
             }
             $piChar++
